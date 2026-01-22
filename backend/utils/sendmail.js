8dcb14512b8fe278b1dc28b4ef372b49218595a7
Revanasidda Nimbal <revanasiddanimbal82@gmail.com>
2026-01-22 21:33:08 +0530

    changed nodemail to brevo api  for otp service

--- backend/utils/sendmail.js
+++ backend/utils/sendmail.js
@@ -1,33 +1,36 @@
-const nodemailer = require("nodemailer");
+const axios = require("axios");
 
 const sendotp = async (email, otp) => {
   try {
-    const transporter = nodemailer.createTransport({
-      host: "smtp-relay.brevo.com",
-      port: 587,
-      secure: false,
-      auth: {
-        user: process.env.BREVO_EMAIL,   // verified sender email
-        pass: process.env.BREVO_SMTP_KEY // SMTP key
-      }
-    });
-
     console.log("Sending OTP to:", email);
 
-    const mailOptions = {
-      from: `MoodMix <${process.env.BREVO_EMAIL}>`,
-      to: email,
-      subject: "Your OTP Code",
-      text: `You have successfully registered to MoodMix.
+    await axios.post(
+      "https://api.brevo.com/v3/smtp/email",
+      {
+        sender: {
+          name: "MoodMix",
+          email: process.env.BREVO_EMAIL,
+        },
+        to: [{ email }],
+        subject: "Your OTP code",
+        textContent: `You have successfully registered to MoodMix.
 
 Your OTP code is: ${otp}
 Valid for 10 minutes.`,
-    };
+      },
+      {
+        headers: {
+          "api-key": process.env.BREVO_SMTP_KEY,
+          "Content-Type": "application/json",
+        },
+        timeout: 10000,
+      },
+    );
 
-    await transporter.sendMail(mailOptions);
+    console.log("OTP email sent successfully");
     return true;
   } catch (err) {
-    console.error("Email error:", err);
+    console.error("Brevo HTTPS error:", err.response?.data || err.message);
     return { error: true };
   }
 };
